18dab1bd35953cb2d095a14a880fadde668efe99
Anton Minakov <int-64@ya.ru>
2021-07-25 14:58:24 +0500

    fix: only words

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 )
 
 func main() {
@@ -14,8 +15,13 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	count := len(strings.Fields(src))
-	fmt.Println(count)
+	counter := 0
+	for _, word := range strings.Fields(src) {
+		if isWord(word) {
+			counter++
+		}
+	}
+	fmt.Println(counter)
 }
 
 func readInput() (src string, err error) {
@@ -25,4 +31,13 @@ func readInput() (src string, err error) {
 		return src, errors.New("missing string to match")
 	}
 	return src, nil
+}
+
+func isWord(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
 }
